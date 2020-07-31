b0172fe7101be1279ddb06a6bc82455ba5117d5b
Jonathan-Vasquez <jhuata@fi.uba.ar>
2020-07-31 20:33:04 -0300

    reseteo total

--- src/main/java/edu/fiuba/algo3/modelo/TipoPregunta.java
+++
@@ -1,8 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-import java.util.ArrayList;
-
-public interface TipoPregunta {
-    //Pre: Las listas opciones y opcionesCorrectas no estan vacias
-    public int calcularPuntaje(ArrayList<Opcion> opcionesJugador, ArrayList<Opcion> opcionesCorrectas);
-}
