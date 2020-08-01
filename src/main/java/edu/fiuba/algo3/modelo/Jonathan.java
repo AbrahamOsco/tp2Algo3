f8b50842f27cd82081b17ae382394b6cdf52689d
Abraham <aosco@fi.uba.ar>
2020-07-31 21:01:04 -0300

    Probando rama de jhonathan

---
+++ src/main/java/edu/fiuba/algo3/modelo/Jonathan.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3.modelo;
+
+public class Jonathan {
+    public string nombre;
+    public int edad;
+
+    public void  aprobar(){
+    }
+    public void dormir(){
+
+    }
+}
--- src/main/java/edu/fiuba/algo3/modelo/Message.java
+++ src/main/java/edu/fiuba/algo3/modelo/Message.java
@@ -1,6 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
-public class Message {
+public class gMessage {
     private final String content;
 
     public Message(String content) {
