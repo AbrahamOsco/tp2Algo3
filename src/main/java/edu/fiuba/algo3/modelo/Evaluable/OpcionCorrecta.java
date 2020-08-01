7146a324dee955646563b2c7c49a8c4dfbb92515
Abraham <aosco@fi.uba.ar>
2020-08-01 20:24:19 -0300

    Movi las clases al directorio Evaluable

--- src/main/java/edu/fiuba/algo3/modelo/Evaluable/OpcionCorrecta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Evaluable/OpcionCorrecta.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.Evaluable;
 
 public class OpcionCorrecta implements Evaluable{
 
--- src/main/java/edu/fiuba/algo3/modelo/Evaluable/OpcionIncorrecta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Evaluable/OpcionIncorrecta.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.Evaluable;
 
 public class OpcionIncorrecta implements Evaluable{
 
