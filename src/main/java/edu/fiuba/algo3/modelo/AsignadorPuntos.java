f8e99567b221906f2cf245873e85f61fe5510bb3
matja <mausqui@fi.uba.ar>
2020-08-01 12:15:57 -0300

    firmas hechas

---
+++ src/main/java/edu/fiuba/algo3/modelo/AsignadorPuntos.java
@@ -0,0 +1,14 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class AsignadorPuntos {
+
+    public void asignarPuntaje (ArrayList<Respuesta> respuestas) {
+
+
+
+
+    }
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class Ronda {
+
+    private Pregunta unaPregunta;
+    private ArrayList<Jugador> jugadores;
+    private int puntajesDeEstaRonda;
+
+    public Ronda(Pregunta unaPregunta, ArrayList<Jugador> jugadores) {
+
+    }
+
+    public void comenzarRonda() {
+
+
+    }
+
+    public void terminarRonda() {
+
+
+
+    }
+
+}
