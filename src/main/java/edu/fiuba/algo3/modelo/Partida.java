c9bbb8bf799528087ed0dede87cbd5ab99ee6b5f
marco-amm <marco_31_33@hotmail.com>
2020-08-01 00:11:26 -0300

    planteo inicial de Jugador y Partida

---
+++ src/main/java/edu/fiuba/algo3/modelo/Partida.java
@@ -0,0 +1,23 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class Partida {
+    private ArrayList<Jugador> jugadores;
+    private ArrayList<Pregunta> preguntas;
+
+    public Partida(){}
+    public void  comenzarPartida(){
+
+        /*for(Pregunta preguntaActual: preguntas){
+            Ronda rondaActual = new Ronda(preguntaActual, this.jugadores);
+            rondaActual.comenzarRonda();
+            rondaActual.terminarRonda();
+        }*/
+    }
+    public void terminarPartida(){
+        //determina al jugador de mayo puntaje y lo informa
+        //permite volver al menu
+    }
+
+}
