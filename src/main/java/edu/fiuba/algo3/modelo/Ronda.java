fc5be38f098e76dfc867e03fcd6a9101639b58ee
matja <mausqui@fi.uba.ar>
2020-08-01 12:32:00 -0300

    firmas hechas

--- src/main/java/edu/fiuba/algo3/modelo/Ronda.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda.java
@@ -4,22 +4,41 @@ import java.util.ArrayList;
 
 public class Ronda {
 
-    private Pregunta unaPregunta;
+    private Pregunta pregunta;
     private ArrayList<Jugador> jugadores;
+    private ArrayList<Respuesta> respuestas;
     private int puntajesDeEstaRonda;
 
-    public Ronda(Pregunta unaPregunta, ArrayList<Jugador> jugadores) {
+
+    public Ronda(Pregunta unaPregunta, ArrayList<Jugador> nuevosJugadores) {
+
+        this.pregunta = unaPregunta;
+        this.jugadores = nuevosJugadores;
 
     }
 
     public void comenzarRonda() {
 
+        for (int i = 0; i < this.jugadores.size(); i++) {
+
+            Turno unTurno = Turno new(this.jugadores.get(i));
+
+            Respuesta unaRespuesta = Respuesta new;
+
+            Respuesta unaRespuesta = unTurno.correrPregunta(this.pregunta);
+
+            this.pregunta.evaluarOpciones(unaRespuesta);
+
+            this.respuestas.add(unaRespuesta);
+        }
 
     }
 
     public void terminarRonda() {
 
+        AsignadorPuntos asignadorPuntos = AsignadorPuntos new;
 
+        asignadorPuntos.asignarPuntaje(this.respuestas);
 
     }
 
