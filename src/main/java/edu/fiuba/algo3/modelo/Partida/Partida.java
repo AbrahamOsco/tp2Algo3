a234d733ca80cdbe4a1ee90d583f78a49e16ffc6
ElBrujas <scamacho@fi.uba.ar>
2020-08-12 15:18:46 -0300

    agregado getters & setters de la clase Partida

--- src/main/java/edu/fiuba/algo3/modelo/Partida/Partida.java
+++ src/main/java/edu/fiuba/algo3/modelo/Partida/Partida.java
@@ -1,13 +1,41 @@
 package edu.fiuba.algo3.modelo.Partida;
 
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
-import edu.fiuba.algo3.modelo.Preguntas.*;
+import edu.fiuba.algo3.modelo.Preguntas.Pregunta;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 
 public class Partida {
-    private ArrayList<Jugador> jugadores;
-    private ArrayList<Pregunta> preguntas;
+	private List<Jugador> jugadores;
+    private Queue<Pregunta> preguntas;
 
-    public Partida(){}
+    public Partida(){
+    	
+    	preguntas = new LinkedList<>();
+    	jugadores = new ArrayList<>();
+    	     
+    }
+    
+    public void agregarJugador (String unNombre) {
+    	jugadores.add(new Jugador(unNombre));
+    }
+    
+    public void setPreguntas (Queue<Pregunta> unasPreguntas) {
+    	preguntas = unasPreguntas;
+    }
+    
+    public Pregunta obtenerSiguientePregunta() {
+    	
+    	return (!preguntas.isEmpty()) ? preguntas.poll():null;
+    		
+    }
+    
+    public List<Jugador> getJugadores() {
+    	return jugadores;
+    }
+    
 }
