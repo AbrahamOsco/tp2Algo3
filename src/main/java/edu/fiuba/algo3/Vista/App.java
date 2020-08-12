b3656c4119df4e4660e9fb29177465857325363f
marco-amm <marco_31_33@hotmail.com>
2020-08-11 22:00:40 -0300

    casmbiando cola de prioridad por codigo con uso stream

--- src/main/java/edu/fiuba/algo3/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/Vista/App.java
@@ -1,10 +1,24 @@
 package edu.fiuba.algo3.Vista;
 
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+import edu.fiuba.algo3.modelo.Opciones.Opcion;
+import edu.fiuba.algo3.modelo.Opciones.OpcionCorrecta;
+import edu.fiuba.algo3.modelo.Opciones.OpcionIncorrecta;
+import edu.fiuba.algo3.modelo.Preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.Preguntas.PreguntaCriterioParcial.PreguntaPuntajeParcialSinIncorrectos;
+import edu.fiuba.algo3.modelo.Turno.Turno;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 /**
  * JavaFX App
--- src/main/java/edu/fiuba/algo3/modelo/modificador/Exclusividad.java
+++ src/main/java/edu/fiuba/algo3/modelo/modificador/Exclusividad.java
@@ -2,35 +2,19 @@ package edu.fiuba.algo3.modelo.modificador;
 
 import edu.fiuba.algo3.modelo.Turno.Turno;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Exclusividad  {
 
     public void aplicarModificador(ArrayList<Turno> unosTurnos) {
-        PriorityQueue<Turno> colaPrioridad = new PriorityQueue<>( (turno1,turno2) -> {
-            if(turno1.obtenerPuntajeParcial() < turno2.obtenerPuntajeParcial() )
-                return 1;
-            else if (turno1.obtenerPuntajeParcial() > turno2.obtenerPuntajeParcial() )
-                return -1;
-            else
-                return 0;
-        }  );
+        Turno turnoMax = (unosTurnos.stream().max(Comparator.comparing(o -> o.obtenerPuntajeParcial())).get());
+        int max = turnoMax.obtenerPuntajeParcial();
+        List<Turno> listaMax = unosTurnos.stream().filter(o -> o.obtenerPuntajeParcial() == max).collect(Collectors.toList());
 
-        for(Turno unTurno : unosTurnos){
-            colaPrioridad.offer(unTurno);
-        }
-        Turno turnoMaximoPuntaje1 = colaPrioridad.poll();
-        Turno turnoMaximoPuntaje2 = colaPrioridad.poll();
-        int puntaje1 = turnoMaximoPuntaje1.obtenerPuntajeParcial();
-        int puntaje2 = turnoMaximoPuntaje2.obtenerPuntajeParcial();
-
-        if(puntaje1 != puntaje2){
-            turnoMaximoPuntaje1.asignarPuntajeFinal(puntaje1*2);
+        if(listaMax.size()==1) {
+            turnoMax.asignarPuntajeFinal(max*2);
         }
 
     }
-
-
-
 }
