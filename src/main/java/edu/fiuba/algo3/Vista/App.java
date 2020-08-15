5a88a603fbae23e91d4af70945bbc0d94c7af579
marco-amm <marco_31_33@hotmail.com>
2020-08-15 20:39:36 -0300

    scena vista pregunta,pruebaita

--- src/main/java/edu/fiuba/algo3/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/Vista/App.java
@@ -1,13 +1,23 @@
 package edu.fiuba.algo3.Vista;
 
+import edu.fiuba.algo3.modelo.Opciones.Opcion;
+import edu.fiuba.algo3.modelo.Opciones.OpcionCorrecta;
+import edu.fiuba.algo3.modelo.Preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.Preguntas.PreguntaCriterioParcial.PreguntaPuntajeParcialSinIncorrectos;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * JavaFX App
@@ -24,6 +34,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show()
          */
+
+        /*
         try{
             URL url = getClass().getResource("/Vistas/VistaInicio.fxml");
             Parent root = FXMLLoader.load(url); //se carga la vista
@@ -33,11 +45,42 @@ public class App extends Application {
             stage.show();
         }catch (IOException e){
             e.printStackTrace();
-        }
+        }*/
+        ArrayList<Opcion> opcionesAPresentar = new ArrayList<>();
+        opcionesAPresentar.add(new OpcionCorrecta("10"));
+        opcionesAPresentar.add(new OpcionCorrecta("15"));
+        opcionesAPresentar.add(new OpcionCorrecta("5"));
+        opcionesAPresentar.add(new OpcionCorrecta("20"));
+        Pregunta multipleChoice = new PreguntaPuntajeParcialSinIncorrectos("numeros?",opcionesAPresentar);
+
+        stage.setScene(this.preguntaMultipleChoiceEscena(multipleChoice));
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    public Scene preguntaMultipleChoiceEscena(Pregunta pregunta){
+        Text consigna = new Text(pregunta.getConsigna());
+        ArrayList<Opcion> opciones = pregunta.getOpciones();
+
+        VBox cajaDeOpciones = new VBox();
+        cajaDeOpciones.setAlignment(Pos.CENTER);
+        cajaDeOpciones.setSpacing(20);
+
+        for(Opcion opcion: opciones){
+            Button botonOpcion = new Button(opcion.getDescripcion());
+            cajaDeOpciones.getChildren().add(botonOpcion);
+            //setea action event q tenga
+        }
+
+        Button botonTerminarTurno = new Button("Terminar Turno");
+
+        VBox vistaGeneral = new VBox(consigna, cajaDeOpciones,  botonTerminarTurno);
+        vistaGeneral.setSpacing(20);
+        vistaGeneral.setAlignment(Pos.CENTER);
+        Scene escenaGeneralPregunta = new Scene(vistaGeneral,500,500);
+        return escenaGeneralPregunta;
+    }
 }
