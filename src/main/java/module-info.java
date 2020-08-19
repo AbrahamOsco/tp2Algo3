34ea49741c23c94459a9ffa3cf45b3d9e83c9fb8
Abraham <aosco@fi.uba.ar>
2020-08-19 01:03:53 -0300

    guardo cambios

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module edu.fiuba.algo {
+module edu.fiuba.algo3 {
     requires javafx.controls;
     requires org.junit.jupiter.api;
     requires javafx.fxml;
+    exports edu.fiuba.algo3.controladores;
     requires javafx.graphics;
     requires com.google.gson;
     exports edu.fiuba.algo3.Vista;
